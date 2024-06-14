f6486329e06f02af980bb3605a53d962e3efb5ab
Angel <estibenejjdkdkkdkd@gmail.com>
2024-06-14 07:34:51 -0500

    Se agregaron las clases (Cliente, Proveedor), se cambiaron los nombres de las variables a minúsculas

--- proyecto/app/models.py
+++ proyecto/app/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Empleado(models.Model):
-    Id_empleado = models.AutoField(primary_key=True)
-    Nombres = models.CharField(max_length=50, verbose_name="Nombres")
-    Apellidos = models.CharField(max_length=50, verbose_name="Apellidos")
-    Telefono = models.CharField(unique=True, max_length=10,verbose_name="Telefono")
+    id_empleado = models.AutoField(primary_key=True)
+    nombres = models.CharField(max_length=50, verbose_name="Nombres")
+    apellidos = models.CharField(max_length=50, verbose_name="Apellidos")
+    telefono = models.CharField(unique=True, max_length=10,verbose_name="Telefono")
 
     class Meta:
         verbose_name = "Empleado"
@@ -15,8 +15,8 @@ class Empleado(models.Model):
 
 
 class Marca(models.Model):
-    Id_marca = models.AutoField(primary_key=True)
-    Nombre = models.CharField(max_length=50, verbose_name="Nombre")
+    id_marca = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=50, verbose_name="Nombre")
 
     class Meta:
         verbose_name = "Marca"
@@ -24,8 +24,8 @@ class Marca(models.Model):
         db_table = "Marca"
 
 class Categoria(models.Model):
-    Id_categoria = models.AutoField(primary_key=True)
-    Nombre = models.CharField(max_length=50, verbose_name="Nombre")
+    id_categoria = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=50, verbose_name="Nombre")
 
     class Meta:
         verbose_name = "Categoria"
@@ -33,10 +33,32 @@ class Categoria(models.Model):
         db_table = "Categoria"
 
 class Presentacion(models.Model):
-    Id_presentacion = models.AutoField(primary_key=True)
-    Descripcion = models.CharField(max_length=50, verbose_name="Descripcion")
+    id_presentacion = models.AutoField(primary_key=True)
+    descripcion = models.CharField(max_length=50, verbose_name="Descripcion")
 
     class Meta:
         verbose_name = "Presentacion"
         verbose_name_plural = "Presentaciones"
         db_table = "Presentacion"
+
+class Cliente(models.Model):
+    cc_cliente = models.IntegerField(primary_key=True)
+    nombres = models.CharField(max_length=200, verbose_name="Nombres")
+    apellidos = models.CharField(max_length=200, verbose_name="Apellidos")
+    telefono = models.CharField(unique=True, max_length=10,verbose_name="Telefono")
+
+    class Meta:
+        verbose_name = "Cliente"
+        verbose_name_plural = "Clientes"
+        db_table = "Cliente"
+
+class Proveedor(models.Model):    
+    id_proveedor = models.AutoField(primary_key=True)
+    nombres = models.CharField(max_length=200, verbose_name="Nombres")
+    telefono = models.CharField(unique=True, max_length=10,verbose_name="Telefono")
+    correo = models.CharField(max_length=200, verbose_name="Correo")
+
+    class Meta:
+        verbose_name = "Proveedor"
+        verbose_name_plural = "Proveedores"
+        db_table = "Proveedor"
