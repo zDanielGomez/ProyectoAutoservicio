0529419f91bb0e05f58101cd656abeb3118fbe1b
Andrey <andreyrimero381gmail.com>
2024-06-21 12:16:26 -0500

    Se agrega listado categoria e implementacion de datatables

--- proyecto/proyecto/urls.py
+++ proyecto/proyecto/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from app.views  import vista1
+from app.views  import *
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("prueba/", include("app.urls"))
+    path('app/', include('app.urls'))
 ]
