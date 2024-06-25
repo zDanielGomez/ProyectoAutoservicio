04ed079044878aca3914e22cc0556c38472134ae
Angel <estibenejjdkdkkdkd@gmail.com>
2024-06-25 09:25:07 -0500

    Se finaliza C.R.U.D de la clase Categoría

--- proyecto/app/urls.py
+++ proyecto/app/urls.py
@@ -5,7 +5,7 @@ from app.views.categoria.views import *
 app_name = 'app'
 urlpatterns = [
     path ("categoria/listar/", CategoriaListView.as_view(), name= 'categoria_lista'),
-    path ("categoria/listar2/", lista_categoria, name= 'categoria_lista2'),
-    path ("categoria/crear/", CategoriaCreateView.as_view(), name= 'categoria_crear')
-
+    path ("categoria/crear/", CategoriaCreateView.as_view(), name= 'categoria_crear'),
+    path ("categoria/editar/<int:pk>/", CategoriaUpdateView.as_view(), name= 'categoria_editar'),
+    path ("categoria/eliminar/<int:pk>/", CategoriaDeleteView.as_view(), name= 'categoria_eliminar')
 ]
--- proyecto/app/views/categoria/views.py
+++ proyecto/app/views/categoria/views.py
@@ -1,9 +1,9 @@
 # from django.contrib.auth.decorators import login_required
-from typing import Any
+
 from django.urls import reverse_lazy
 from django.views.decorators.csrf import csrf_protect,csrf_exempt
 from django.http import JsonResponse
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView,UpdateView,DeleteView
 from django.utils.decorators import method_decorator
 from django.shortcuts import render,redirect
 from app.models import *
@@ -37,6 +37,8 @@ class CategoriaListView(ListView):
     def get_context_data(self, **kwargs):
         context= super().get_context_data(**kwargs)
         context['titulo'] = 'Listado de Categorias'
+        context['crear_url'] = reverse_lazy('app:categoria_crear')
+        context['entidad'] = 'Categorías'
         return context
     
 class CategoriaCreateView(CreateView):
@@ -48,5 +50,34 @@ class CategoriaCreateView(CreateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['titulo'] = 'Crear Categoria'
+        context['entidad'] = 'Categorías'
+        context['listar_url'] = reverse_lazy('app:categoria_lista')
+        
+        return context
+
+class CategoriaUpdateView(UpdateView):
+    model = Categoria
+    form_class = CategoriaForm
+    template_name = 'categoria/crear.html'
+    success_url = reverse_lazy('app:categoria_lista')
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['titulo'] = 'Editar Categoria'
+        context['entidad'] = 'Categorías'
+        context['listar_url'] = reverse_lazy('app:categoria_lista')
+        
+        return context
+    
+class CategoriaDeleteView(DeleteView):
+    model = Categoria
+    template_name = 'categoria/eliminar.html'
+    success_url = reverse_lazy('app:categoria_lista')
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['titulo'] = 'Eliminar Categoria'
+        context['entidad'] = 'Categorías'
+        context['listar_url'] = reverse_lazy('app:categoria_lista')
         
         return context
