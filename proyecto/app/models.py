e4bf59770c02d1d515d658b28d1495c3a75f852c
zDanielGomez <dg154906@gmail.com>
2024-07-09 18:09:54 -0500

    Se arreglo la base de datos

--- proyecto/app/models.py
+++ proyecto/app/models.py
@@ -3,7 +3,6 @@ from datetime import datetime
 
 # Se agrego la tabla Empleado con sus atributos y metodos
 class Empleado(models.Model):
-    id_empleado = models.AutoField(primary_key=True)
     nombres = models.CharField(max_length=50, verbose_name="Nombres")
     apellidos = models.CharField(max_length=50, verbose_name="Apellidos")
     telefono = models.CharField(unique=True, max_length=10,verbose_name="Telefono")
@@ -18,7 +17,6 @@ class Empleado(models.Model):
 
 # Se agrego la tabla Marca con sus atributos y metodos
 class Marca(models.Model):
-    id_marca = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=50, verbose_name="Nombre")
     
     def __str__(self):
@@ -31,7 +29,6 @@ class Marca(models.Model):
 
 # Se agrego la tabla Categoria con sus atributos y metodos
 class Categoria(models.Model):
-    id_categoria = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=50, verbose_name="Nombre", unique=True)
     descripcion = models.CharField(max_length=200, verbose_name="Descripcion", blank=True, null=True)
 
@@ -45,7 +42,6 @@ class Categoria(models.Model):
 
 # Se agrego la tabla Presentacion con sus atributos y metodos
 class Presentacion(models.Model):
-    id_presentacion = models.AutoField(primary_key=True)
     descripcion = models.CharField(max_length=50, verbose_name="Descripcion")
     
     def __str__(self):
@@ -58,7 +54,7 @@ class Presentacion(models.Model):
 
 # Se agrego la tabla Cliente con sus atributos y metodos
 class Cliente(models.Model):
-    cc_cliente = models.IntegerField(primary_key=True)
+    cc_cliente = models.IntegerField(unique=True)
     nombres = models.CharField(max_length=200, verbose_name="Nombres")
     apellidos = models.CharField(max_length=200, verbose_name="Apellidos")
     telefono = models.CharField(unique=True, max_length=10,verbose_name="Telefono")
@@ -73,7 +69,6 @@ class Cliente(models.Model):
 
 # Se agrego la tabla Proveedor con sus atributos y metodos
 class Proveedor(models.Model):    
-    id_proveedor = models.AutoField(primary_key=True)
     nombres = models.CharField(max_length=200, verbose_name="Nombres")
     telefono = models.CharField(unique=True, max_length=10,verbose_name="Telefono")
     correo = models.CharField(max_length=200, verbose_name="Correo")
@@ -88,13 +83,12 @@ class Proveedor(models.Model):
 
 # Se agrego la tabla Producto con sus atributos y metodos
 class Producto(models.Model):    
-    id_producto = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=200, verbose_name="Nombre")
     descripcion = models.CharField(max_length=50, verbose_name="Descripcion")
     cantidad = models.IntegerField(verbose_name="Cantidad")
-    id_marca = models.ForeignKey(Marca,on_delete=models.CASCADE)
-    id_categoria = models.ForeignKey(Categoria,on_delete=models.CASCADE)
-    id_presentacion = models.ForeignKey(Presentacion,on_delete=models.CASCADE)
+    marca = models.ForeignKey(Marca,on_delete=models.CASCADE)
+    categoria = models.ForeignKey(Categoria,on_delete=models.CASCADE)
+    presentacion = models.ForeignKey(Presentacion,on_delete=models.CASCADE)
     precio = models.DecimalField(default=0.00,max_digits=9 ,decimal_places=2)
     
     def __str__(self):
@@ -107,15 +101,14 @@ class Producto(models.Model):
 
 # Se agrego la tabla Venta con sus atributos y metodos
 class Venta(models.Model):    
-    id_venta = models.AutoField(primary_key=True)
     fecha_venta = models.DateField(default=datetime.now)  
-    cc_cliente = models.ForeignKey(Cliente,on_delete=models.CASCADE,verbose_name="Cliente")
-    id_empleado = models.ForeignKey(Empleado,on_delete=models.CASCADE,verbose_name="Empleado")
-    id_producto = models.ForeignKey(Producto,on_delete=models.CASCADE)
+    cliente = models.ForeignKey(Cliente,on_delete=models.CASCADE,verbose_name="Cliente")
+    empleado = models.ForeignKey(Empleado,on_delete=models.CASCADE,verbose_name="Empleado")
+    producto = models.ForeignKey(Producto,on_delete=models.CASCADE)
     total_venta = models.DecimalField(default=0.00,max_digits=9 ,decimal_places=2)
     
     def __str__(self):
-        return f"{self.id_venta}"
+        return f"{self.fecha_venta}"
     
     class Meta:
         verbose_name = "Venta"
@@ -124,14 +117,13 @@ class Venta(models.Model):
 
 # Se agrego la tabla Compra con sus atributos y metodos
 class Compra(models.Model):
-    id_compra = models.AutoField(primary_key=True)
     fecha_compra = models.DateField(default=datetime.now)  
-    id_proveedor = models.ForeignKey(Proveedor,on_delete=models.CASCADE)
-    id_producto = models.ForeignKey(Producto,on_delete=models.CASCADE)
+    proveedor = models.ForeignKey(Proveedor,on_delete=models.CASCADE)
+    producto = models.ForeignKey(Producto,on_delete=models.CASCADE)
     total_compra = models.DecimalField(default=0.00,max_digits=9 ,decimal_places=2)
     
     def __str__(self):
-        return f"{self.id_compra}"
+        return f"{self.fecha_compra}"
     
     class Meta:
         verbose_name = "Compra"
