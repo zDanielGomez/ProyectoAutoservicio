63a669bb6225166f84dbb92fa3b7254c08ce75cf
Andrey Romero <andreyrimero381@gmail.com>
2024-07-26 16:15:01 -0500

    Se implemento diseño en la recuperacion de la contraseña

--- proyecto/proyecto/settings.py
+++ proyecto/proyecto/settings.py
@@ -132,3 +132,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL  ="login"
 LOGIN_REDIRECT_URL ="/dashboard"
 LOGOUT_REDIRECT_URL ="/login/"
+
+# Olvido su contraseña
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com' 
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'rinconautoservicio@gmail.com' 
+EMAIL_HOST_PASSWORD = 'znec jalj aqhj wpla'
